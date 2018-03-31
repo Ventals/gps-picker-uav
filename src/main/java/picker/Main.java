2486a73ae467da267118b4dc71bea28ef89cbfdf
HustonBack <samiliak.direct@gmail.com>
2018-04-01 01:14:10 +0300

    controller

--- src/main/java/picker/Main.java
+++ src/main/java/picker/Main.java
@@ -1,8 +1,18 @@
 package picker;
 
+import picker.engine.Controller;
+
+import java.io.IOException;
+
 public class Main {
 
-    public static void main(String[] args){
-
+    public static void main(String[] args) throws IOException {
+        double[][] result = new Controller().getPhotoCoords();
+//        for (double[] pair : result){
+//            for (double item : pair){
+//                System.out.print(item + " ");
+//            }
+//            System.out.println();
+//        }
     }
 }
--- src/main/java/picker/engine/Controller.java
+++ src/main/java/picker/engine/Controller.java
@@ -1,5 +1,47 @@
 package picker.engine;
 
-public class Controller {
+import picker.engine.model.Data;
+import picker.engine.util.MathUtils;
+import picker.module.ParseFile;
+import sun.management.snmp.jvmmib.EnumJvmMemPoolCollectThreshdSupport;
 
+import java.io.IOException;
+
+public class Controller {
+    public double[][] getPhotoCoords () throws IOException {
+        Data data = new ParseFile("input.csv").getParsedData(5);
+        double[] centralCoords = getCentralCoords(data);
+        for (double item : centralCoords) {
+            System.out.print(item + " ");
+        }
+//        System.out.println("\n ^ central ------");
+        double[][] pitchCorrection = MathUtils.getPitchCorrections(data);
+//        for (double[] pair : pitchCorrection){
+//            for (double item : pair){
+//                System.out.print(item + " ");
+//            }
+//            System.out.println("\n^ pitch ------");
+//        }
+        double[][] rollCorrection = MathUtils.getRollCorrections(data);
+//        for (double[] pair : pitchCorrection){
+//            for (double item : pair){
+//                System.out.print(item + " ");
+//            }
+//            System.out.println("\n^ roll ------");
+//        }
+        double[][] result = new double[4][2];
+        for (int i = 0; i < rollCorrection.length; i++){
+            double x = ( centralCoords[0] + pitchCorrection[i][0] + rollCorrection[i][0] ) * Math.cos(data.getYaw());
+            double y = ( centralCoords[1] + pitchCorrection[i][1] + rollCorrection[i][1] ) * Math.sin(data.getYaw());
+            result[i] = new double[]{x, y};
+        }
+        return result;
+    }
+
+    private double[] getCentralCoords (Data data) {
+        return new double[]{
+                data.getH() * Math.sin(data.getRoll()),
+                data.getH() * Math.sin(data.getPitch())
+        };
+    }
 }
